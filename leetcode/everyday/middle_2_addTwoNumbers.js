a63a3c67fd70531cbd9787ad748a884f77d81736
18321844291 <wen.li@unicareer.com>
2021-02-16 22:26:15 +0800

    feat: 每日刷题(2)- 两数相加

---
+++ leetcode/everyday/middle_2_addTwoNumbers.js
@@ -0,0 +1,104 @@
+/**
+ * 两数相加
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let r1 = l1,
+    r2 = l2;
+  let isAddOne = 0,
+    lastNode = null;
+  let result = new ListNode(0);
+  while (r1 && r2) {
+    let singleSum = r1.val + r2.val + isAddOne;
+    if (singleSum > 9) {
+      // 进1
+      isAddOne = 1;
+    } else {
+      isAddOne = 0;
+    }
+    let currentNode = new ListNode(singleSum % 10, null);
+    if (lastNode) {
+      lastNode.next = currentNode;
+    } else {
+      result.next = currentNode;
+    }
+    lastNode = currentNode;
+    r1 = r1.next;
+    r2 = r2.next;
+  }
+  while (r1) {
+    let singleSum = r1.val + isAddOne;
+    if (singleSum > 9) {
+      // 进1
+      isAddOne = 1;
+    } else {
+      isAddOne = 0;
+    }
+    let currentNode = new ListNode(singleSum % 10, null);
+    if (lastNode) {
+      lastNode.next = currentNode;
+    } else {
+      result.next = currentNode;
+    }
+    lastNode = currentNode;
+    r1 = r1.next;
+  }
+  while (r2) {
+    let singleSum = r2.val + isAddOne;
+    if (singleSum > 9) {
+      // 进1
+      isAddOne = 1;
+    } else {
+      isAddOne = 0;
+    }
+    let currentNode = new ListNode(singleSum % 10, null);
+    if (lastNode) {
+      lastNode.next = currentNode;
+    } else {
+      result.next = currentNode;
+    }
+    lastNode = currentNode;
+    r2 = r2.next;
+  }
+  if(isAddOne){
+    lastNode.next = new ListNode(1);
+  }
+  return result.next;
+};
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+(async () => {
+  // 2 4 3
+  // 5 6 4
+  let l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+  let l2 = new ListNode(
+    9,
+    new ListNode(
+      9,
+      new ListNode(
+        9,
+        new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+      )
+    )
+  );
+  // 342 +
+  // 465 =
+  // 807
+
+  // 9999
+  // 9999999
+  // 8999001
+  addTwoNumbers(l1, l2);
+})();
